ff600694f2bae8f8efa16356329c4f2e8a3a6706
Prachi <prachipramodkumar15@gmail.com>
2024-04-20 02:15:43 +0530

    vercel file added

--- Backend/app.js
+++ Backend/app.js
@@ -53,6 +53,8 @@ app.get("/api/cities", (req, res) => {
   });
 });
 
+app.get("", (req, res) => {
+  res.send("hello world")})
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
